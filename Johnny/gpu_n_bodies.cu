312d927ff31d6b0580e53141520a08750a1c028f
Johnny Jiang <autoruncd.johnny@gmail.com>
2019-03-30 17:15:33 -0400

    minor changes

--- Johnny/gpu_n_bodies.cu
+++ Johnny/gpu_n_bodies.cu
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <math.h>
 
-//remove this if run on ug machine
+//add this if compiled by visual studio
 #include <device_launch_parameters.h>
 
 #define G 6.67e-2f
@@ -28,14 +28,16 @@ void initialize(float3* h_s, float3* h_v, float3* h_a, float dt, int num_bodies)
 
 	for (i = 0; i < num_bodies; i++) {
 		for (j = 0; j < num_bodies; j++) {
-			r.x = h_s[j].x - h_s[i].x;
-			r.y = h_s[j].y - h_s[i].y;
-			r.z = h_s[j].z - h_s[i].z;
-			r2 = r.x*r.x + r.y*r.y + r.z*r.z + EPS;
-			inv_r3 = 1.0f / sqrtf(r2*r2*r2);
-			h_a[i].x += inv_r3 * r.x*MASS;
-			h_a[i].y += inv_r3 * r.y*MASS;
-			h_a[i].z += inv_r3 * r.z*MASS;
+			if (i != j) {
+				r.x = h_s[j].x - h_s[i].x;
+				r.y = h_s[j].y - h_s[i].y;
+				r.z = h_s[j].z - h_s[i].z;
+				r2 = r.x*r.x + r.y*r.y + r.z*r.z + EPS;
+				inv_r3 = 1.0f / sqrtf(r2*r2*r2);
+				h_a[i].x += inv_r3 * r.x*MASS;
+				h_a[i].y += inv_r3 * r.y*MASS;
+				h_a[i].z += inv_r3 * r.z*MASS;
+			}
 		}
 	}
 
@@ -49,41 +51,48 @@ void initialize(float3* h_s, float3* h_v, float3* h_a, float dt, int num_bodies)
 void cpu_func(float3* h_s, float3* h_v, float3* h_a, float dt, int num_bodies, int num_iteration) {
 	int i, j,k;
 	float3 r;
-	float r2, inv_r3;
+	float r2, inv_r3,total_t=0;
 	FILE* fp = fopen("pos_cpu.txt","w");
-	
+	FILE* fp1 = fopen("accel_cpu.txt", "w");
 	for (i = 0; i < num_iteration; i++) {
-		
+		clock_t t0 = clock();
 		for (j = 0; j < num_bodies; j++) {
-			
 			h_s[j].x +=  h_v[j].x*dt;
 			h_s[j].y +=  h_v[j].y*dt;
 			h_s[j].z +=  h_v[j].z*dt;
 			h_v[j].x +=  h_a[j].x*dt;
 			h_v[j].y +=  h_a[j].y*dt;
 			h_v[j].z +=  h_a[j].z*dt;
-			fprintf(fp, "%.6f %.6f %.6f\n", h_s[j].x, h_s[j].y, h_s[j].z);
 		}
-		
 		for (j = 0; j< num_bodies; j++) {
 			h_a[j].x = 0;
 			h_a[j].y = 0;
 			h_a[j].z = 0;
 			for (k = 0; k < num_bodies; k++) {
-				r.x = h_s[k].x - h_s[j].x;
-				r.y = h_s[k].y - h_s[j].y;
-				r.z = h_s[k].z - h_s[j].z;
-				r2 = r.x*r.x + r.y*r.y + r.z*r.z + EPS;
-				inv_r3 = 1.0f / sqrtf(r2*r2*r2);
-				h_a[j].x += inv_r3 * r.x*MASS;
-				h_a[j].y += inv_r3 * r.y*MASS;
-				h_a[j].z += inv_r3 * r.z*MASS;
-			}
+				if (j != k) {
+					r.x = h_s[k].x - h_s[j].x;
+					r.y = h_s[k].y - h_s[j].y;
+					r.z = h_s[k].z - h_s[j].z;
+					r2 = r.x*r.x + r.y*r.y + r.z*r.z + EPS;
+					inv_r3 = 1.0f / sqrtf(r2*r2*r2);
+					h_a[j].x += inv_r3 * r.x*MASS;
+					h_a[j].y += inv_r3 * r.y*MASS;
+					h_a[j].z += inv_r3 * r.z*MASS;
+					//printf("cpu: ax[%d]=%.6f\n", j, h_a[j].x);
+				}
+			}	
 		}
-		
+		clock_t t1 = clock();
+		printf("iteration %d, time cost %.6f\n", i, (float)(t1 - t0) / (float)CLOCKS_PER_SEC);
+		total_t += (float)(t1 - t0) / (float)CLOCKS_PER_SEC;
+		for (j = 0; j < num_bodies; j++) {
+			fprintf(fp1, "%.6f %.6f %.6f\n", h_a[j].x, h_a[j].y, h_a[j].z);
+			fprintf(fp, "%.6f %.6f %.6f\n", h_s[j].x, h_s[j].y, h_s[j].z);
+		}
 	}
+	printf("total time %.6f, avg time per iteration %.6f\n", total_t, total_t / (float)num_iteration);
 	fclose(fp);
-
+	fclose(fp1);
 }
 
 
@@ -92,20 +101,30 @@ __global__ void initialize(float3* s,float3* v,float3* a,int num_bodies){
 
 }*/
 
-__device__ void force_calc(float3 s, float3* a) {
+__device__ void force_calc(float3 s,int check_idx,int tile, int num_bodies, float3* a) {
 	__shared__ float3 shared_s[BLOCK_DIM];
-	int i;
+	int i,N=BLOCK_DIM;
 	float3 r;
 	float r2, inv_r3;
-	for (i = 0; i < blockDim.x; i++) {
-		r.x = shared_s[i].x - s.x;
-		r.y = shared_s[i].y - s.y;
-		r.z = shared_s[i].z - s.z;
-		r2 = r.x*r.x + r.y*r.y + r.z*r.z + EPS;
-		inv_r3 = 1.0f / sqrtf(r2*r2*r2);
-		(*a).x += inv_r3 * r.x*MASS*G;
-		(*a).y += inv_r3 * r.y*MASS*G;
-		(*a).z += inv_r3 * r.z*MASS*G;
+	if ((tile + 1)*BLOCK_DIM > num_bodies) {
+		N = num_bodies % BLOCK_DIM;
+	}
+
+	for (i = 0; i < N; i++) {
+		if (i!=check_idx) {
+			r.x = shared_s[i].x - s.x;
+			r.y = shared_s[i].y - s.y;
+			r.z = shared_s[i].z - s.z;
+			r2 = r.x*r.x + r.y*r.y + r.z*r.z + EPS;
+			inv_r3 = 1.0f / sqrtf(r2*r2*r2);
+			(*a).x += inv_r3 * r.x*MASS;
+			(*a).y += inv_r3 * r.y*MASS;
+			(*a).z += inv_r3 * r.z*MASS;
+			//printf("gpu a.x=%.6f\n", (*a).x);
+		}
+		else {
+			//printf("sggsgsgs\n");
+		}
 	}
 }
 
@@ -113,18 +132,24 @@ __device__ void force_calc(float3 s, float3* a) {
 __global__ void accel_update(float3* s, float3* a, int num_bodies) {
 	__shared__ float3 shared_s[BLOCK_DIM];
 	int gidx = blockDim.x*blockIdx.x + threadIdx.x;
-	float3 accel = { 0.0f,0.0f,0.0f }, myPos = s[gidx];
+	float3 accel = { 0.0f,0.0f,0.0f }, myPos;
 	int idx, i, tile = 0;
 
 	if (gidx < num_bodies) {
-		for (i = 0; i < num_bodies; i += blockDim.x) {
-			idx = tile * blockDim.x + threadIdx.x;
+		myPos = s[gidx];
+	
+		
+	
+	for (i = 0; i < num_bodies; i += blockDim.x) {
+		idx = tile * blockDim.x + threadIdx.x;
+		if (idx <= num_bodies) {
 			shared_s[threadIdx.x] = s[idx];
 			__syncthreads();
-			force_calc(myPos, &accel);
+			force_calc(myPos,threadIdx.x,tile,num_bodies,&accel);
 			__syncthreads();
 			tile++;
 		}
+	}
 	
 	a[gidx] = accel;
 	}
@@ -145,19 +170,27 @@ __global__ void pos_update(float3* s, float3* v, float3* a, float dt, int num_bo
 void gpu_func(float3* s, float3* v, float3* a, float3* h_s, float3* h_a, float dt, int num_bodies, int num_iteration) {
 	int i, j;
 	FILE *fp = fopen("pos.txt", "w");
-
-
+	FILE *fp1 = fopen("accel.txt", "w");
+	float total_t = 0;
 	for (i = 0; i < num_iteration; i++) {
+		clock_t t0 = clock();
 		pos_update <<<(num_bodies + BLOCK_DIM - 1) / BLOCK_DIM, BLOCK_DIM >>> (s, v, a, dt, num_bodies);
 		cudaDeviceSynchronize();
-		accel_update <<<(num_bodies + BLOCK_DIM - 1) / BLOCK_DIM, BLOCK_DIM >>> (s, v, num_bodies);
+		accel_update <<<(num_bodies + BLOCK_DIM - 1) / BLOCK_DIM, BLOCK_DIM >>> (s, a, num_bodies);
 		cudaDeviceSynchronize();
+		clock_t t1 = clock();
+		printf("iteration %d, time cost %.6f\n", i, (float)(t1 - t0) /(float)CLOCKS_PER_SEC);
+		total_t += (float)(t1 - t0) / (float)CLOCKS_PER_SEC;
 		cudaMemcpy(h_s, s, num_bodies * sizeof(float3), cudaMemcpyDeviceToHost);
-		//cudaMemcpy( h_a, a, num_bodies * sizeof(float3), cudaMemcpyDeviceToHost );
-		for (j = 0; j < num_bodies; j++)
+		cudaMemcpy( h_a, a, num_bodies * sizeof(float3), cudaMemcpyDeviceToHost );
+		for (j = 0; j < num_bodies; j++) {
 			fprintf(fp, "%.6f %.6f %.6f\n", h_s[j].x, h_s[j].y, h_s[j].z);
+			fprintf(fp1, "%.6f %.6f %.6f\n", h_a[j].x, h_a[j].y, h_a[j].z);
+		}
 	}
+	printf("total time %.6f, avg time per iteration %.6f\n", total_t, total_t/(float)num_iteration);
 	fclose(fp);
+	fclose(fp1);
 }
 
 
@@ -219,8 +252,9 @@ int main(int argc, char *argv[]) {
 		printf("%s in %s at line %d\n", cudaGetErrorString(err22), __FILE__, __LINE__);
 	}
 	//cpu code
+	printf("cpu code is running....\n");
 	cpu_func(h_s,h_v,h_a,dt,num_bodies, num_iteration);
-
+	printf("gpu code is running....\n");
 	//run gpu code
 	gpu_func(s, v, a, h_s, h_a, dt, num_bodies, num_iteration);
 
